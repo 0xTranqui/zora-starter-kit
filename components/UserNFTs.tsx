74dd6b47e7a3f98c4d19de3b8649b2936102b21a
0xTranqui <thinkandchill11@gmail.com>
2022-07-10 17:32:10 -0400

    formatting

--- components/UserNFTs.tsx
+++ components/UserNFTs.tsx
@@ -3,14 +3,19 @@ const UserNFTs = ({ nfts }) => {
     return (
         <>
             <div className="flex flex-row flex-wrap  justify-center text-center w-full ">
-                <div className="border-2 border-solid border-red-500 text-white w-2/12">
-                    Address
-                </div>                
-                <div className="border-2 border-solid border-red-500 text-white w-2/12">
-                    My Balance
+                <div className="text-white text-4xl pb-10">
+                    My NFTs
                 </div>
-                <div className="border-2 border-solid border-red-500 text-white w-5/12">
-                    Name
+                <div className="flex flex-row flex-wrap  justify-center w-full">
+                    <div className="border-2 border-solid border-red-500 text-white w-2/12">
+                        Address
+                    </div>                
+                    <div className="border-2 border-solid border-red-500 text-white w-2/12">
+                        My Balance
+                    </div>
+                    <div className="border-2 border-solid border-red-500 text-white w-5/12">
+                        Name
+                    </div>
                 </div>
 
             </div>
@@ -23,7 +28,7 @@ const UserNFTs = ({ nfts }) => {
                         return (
                             <div key={nft.collectionAddress} className="text-center text-white w-full flex flex-row flex-wrap justify-center">
                                 <a
-                                href={`https://etherscan.io/address/${nft}`}
+                                href={`https://etherscan.io/address/${nft.collectionAddress}`}
                                 className="border-2 border-solid border-red-500 w-2/12 underline hover:text-green-300"
                                 >
                                     {"" + (nft.collectionAddress.substring(0, 4)) + "..." + (nft.collectionAddress.substring(nft.collectionAddress.length - 4))}
@@ -39,13 +44,62 @@ const UserNFTs = ({ nfts }) => {
                     }
                 }
                 ) : (
-                    <div className="text-white">
-                    </div>
+                <div className="text-center text-white w-full flex flex-row flex-wrap justify-center">
+                    <div className="border-2 border-solid border-red-500 w-2/12 ">
+                        n/a
+                    </div>        
+                    <div className="border-2 border-solid border-red-500 w-2/12 ">
+                        0  
+                    </div>                                             
+                    <div className="border-2 border-solid border-red-500 w-5/12">
+                        n/a
+                    </div>                     
+                </div>
                 )
             }
+
+            <div className="border-2 border-solid border-blue-500 mt-10 flex flex-row flex-wrap  justify-center text-center w-full ">
+                
+                <div className="text-white text-4xl pb-10">
+                    Collection Lookup
+                </div>
+                <div className="flex flex-col flex-wrap content-center w-full">
+                    <div className="border-2 border-solid border-red-500 text-white w-3/12">
+                        Address
+                    </div>                
+                    <div className="border-2 border-solid border-red-500 text-white w-3/12">
+                        Name
+                    </div>
+                    <div className="border-2 border-solid border-red-500 text-white w-3/12">
+                        Current Supply
+                    </div>
+                    <div className="border-2 border-solid border-red-500 text-white w-3/12">
+                        Owner Count
+                    </div>
+                    <div className="border-2 border-solid border-red-500 text-white w-3/12">
+                        Sales Volume (ETH)
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 
 }
 
 export default UserNFTs
+
+// query MyQuery {
+//     collections(where: {collectionAddresses: "0x8427e46826a520b1264B55f31fCB5DDFDc31E349"}) {
+//       nodes {
+//         name
+//         address
+//       }
+//     }
+//     aggregateStat {
+//       ownerCount(where: {collectionAddresses: "0x8427e46826a520b1264B55f31fCB5DDFDc31E349"})
+//       nftCount(where: {collectionAddresses: "0x8427e46826a520b1264B55f31fCB5DDFDc31E349"})
+//       salesVolume(where: {collectionAddresses: "0x8427e46826a520b1264B55f31fCB5DDFDc31E349"}) {
+//         chainTokenPrice
+//       }
+//     }
--- pages/indexer.tsx
+++ pages/indexer.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { ZDK, ZDKNetwork, ZDKChain } from "@zoralabs/zdk";
-import { NFTPreview, MediaConfiguration } from '@zoralabs/nft-components';
 import { Networks, Strategies } from "@zoralabs/nft-hooks"
 
 import { useAccount } from 'wagmi'
@@ -29,6 +28,101 @@ const zdk = new ZDK(zdkArgs) // All arguments are optional
 const zdkStrategyMainnet = new Strategies.ZDKFetchStrategy(
   Networks.MAINNET
 )
+
+const appendContractName = (arr, address) => {
+  // runs a check for ENS NFTs which return a name of null unless cleaned
+  const ensAddress = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85"
+  if ( address === ensAddress ) {
+    const contractName = "ENS"
+    return contractName
+  } else {
+    const contractName = arr.token.tokenContract["name"]
+    return contractName
+  }
+}
+
+const nftCounter = (arr, key) => {
+  let arr2 = [];
+    
+  arr.forEach((x)=>{
+        
+    // Checking if there is any object in arr2
+    // which contains the key value
+      if(arr2.some((val)=>{ return val[key] == x.token[key] })){
+          
+        // If yes! then increase the count by 1
+        arr2.forEach((k)=>{
+          if(k[key] === x.token[key]){ 
+            k["count"]++
+          }
+      })
+          
+      }else{
+        // If not! Then create a new object initialize 
+        // it with the present iteration key's value and 
+        // set the count to 1
+        let a = {}
+        a[key] = x.token[key]
+        a["name"] = appendContractName(x, x.token.collectionAddress)
+        a["count"] = 1        
+        arr2.push(a);
+      }
+  })
+    
+  const sortedArray = arr2.sort((a, b) => {
+    return b.count - a.count
+  })
+
+  return sortedArray
+}
+
+const Api: NextPage = () => {
+  const [userNFTs, setUserNFTs] = useState({});
+  const currentUserNFTs = userNFTs ? userNFTs: "nothing";
+
+  const { address: account } = useAccount(); 
+  const currentUserAddress = account ? account : ""
+  console.log("currentUseraddress: ", currentUserAddress)
+
+  const tokensResponse = async(args) => {
+    const zdkResponseTokens = await (await zdk.tokens(args)).tokens.nodes
+    console.log("zdkResponseTokens", zdkResponseTokens)
+    console.log("nftCounter", nftCounter(zdkResponseTokens, "collectionAddress"))
+    const myNFTs = nftCounter(zdkResponseTokens, "collectionAddress")
+    setUserNFTs(myNFTs)
+  }
+
+  const tokensArgs = {
+    where: {
+      ownerAddresses: [currentUserAddress]
+    },
+    pagination: {
+      limit: 200
+    },
+    includeFullDetails: false
+  }
+
+
+  useEffect(() => {
+    if(!!userNFTs) {
+    tokensResponse(tokensArgs)
+    }},
+    [currentUserAddress]
+  )
+
+
+  return (
+    <div className='flex flex-col justify-center h-screen min-h-screen'>
+      <Header />
+      <main className="w-full flex flex-row flex-wrap justify-center self-center items-center">        
+      <UserNFTs nfts={currentUserNFTs} />
+      </main>
+    </div>
+  )
+}
+
+export default Api
+
 
 // const keyChecker = (obj) => {
 
@@ -76,104 +170,3 @@ const zdkStrategyMainnet = new Strategies.ZDKFetchStrategy(
 //   // } 
 
 // }
-
-const appendContractName = (arr, address) => {
-  // runs a check for ENS NFTs which return a name of null unless cleaned
-  const ensAddress = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85"
-  if ( address === ensAddress ) {
-    const contractName = "ENS"
-    return contractName
-  } else {
-    const contractName = arr.token.tokenContract["name"]
-    return contractName
-  }
-}
-
-const nftCounter = (arr, key) => {
-  let arr2 = [];
-    
-  arr.forEach((x)=>{
-    console.log("what is x: ", x.token[key])
-        
-    // Checking if there is any object in arr2
-    // which contains the key value
-      if(arr2.some((val)=>{ return val[key] == x.token[key] })){
-          
-        // If yes! then increase the count by 1
-        arr2.forEach((k)=>{
-          if(k[key] === x.token[key]){ 
-            k["count"]++
-          }
-      })
-          
-      }else{
-        // If not! Then create a new object initialize 
-        // it with the present iteration key's value and 
-        // set the count to 1
-        let a = {}
-        a[key] = x.token[key]
-        a["name"] = appendContractName(x, x.token.collectionAddress)
-        a["count"] = 1        
-        arr2.push(a);
-      }
-  })
-    
-  const sortedArray = arr2.sort((a, b) => {
-    return b.count - a.count
-  })
-
-  console.log("sortedArray", sortedArray)
-  return sortedArray
-}
-
-const Api: NextPage = () => {
-  const [userNFTs, setUserNFTs] = useState({});
-  const currentUserNFTs = userNFTs ? userNFTs: "nothing";
-
-  const { address: account } = useAccount(); 
-  const currentUserAddress = account ? account : ""
-  console.log("currentUseraddress: ", currentUserAddress)
-
-  const tokensResponse = async(args) => {
-    const response = await (await zdk.tokens(args)).tokens.nodes
-    console.log("response", response)
-    console.log("nftCounter", nftCounter(response, "collectionAddress"))
-    const myNFTs = nftCounter(response, "collectionAddress")
-    setUserNFTs(myNFTs)
-  }
-
-  const tokensArgs = {
-    where: {
-      ownerAddresses: [currentUserAddress]
-    },
-    pagination: {
-      limit: 200
-    },
-    includeFullDetails: false
-  }
-
-  useEffect(() => {
-    tokensResponse(tokensArgs)
-    },
-    []
-  )
-
-  useEffect(() => {
-    if(!!userNFTs) {
-    tokensResponse(tokensArgs)
-    }},
-    [currentUserAddress]
-  )
-
-
-  return (
-    <div className='flex flex-col justify-center h-screen min-h-screen'>
-      <Header />
-      <main className="w-full flex flex-row flex-wrap items-center">        
-      <UserNFTs nfts={currentUserNFTs} />
-      </main>
-    </div>
-  )
-}
-
-export default Api
