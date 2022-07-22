8796375aa161a9877fb8c2b24991137131aab414
0xTranqui <thinkandchill11@gmail.com>
2022-07-22 03:14:46 -0400

    build fix 5

---
+++ components/Auctions/AuctionForNFT.tsx
@@ -0,0 +1,87 @@
+import { Header } from "../Header";
+import { useContractRead, useAccount } from "wagmi";
+import { AsksV1_1Interface } from "@zoralabs/v3/dist/typechain/AsksV1_1"
+import * as asksAddresses from "@zoralabs/v3/dist/addresses/1.json"
+import { abi } from "@zoralabs/v3/dist/artifacts/AsksV1_1.sol/AsksV1_1.json"
+import { useState, useEffect } from "react";
+import { ReadContractResult } from "@wagmi/core";
+import { BigNumber, utils } from "ethers";
+
+export const AskForNFT = (nft) => {
+
+    interface askForNFT {
+        tokenContract: any,
+        tokenId: any
+    }
+
+    const [askForNFT, setAskForNFT] = useState<askForNFT>({
+        "tokenContract": "0x7e6663E45Ae5689b313e6498D22B041f4283c88A",
+        "tokenId": "1",
+    })
+
+    // AsksV1_1 askForNFT read call
+    const { data, isLoading, isSuccess, isFetching  } = useContractRead({
+        addressOrName: asksAddresses.AsksV1_1,
+        contractInterface: abi,
+        functionName: 'askForNFT',
+        args: [
+            askForNFT.tokenContract,
+            askForNFT.tokenId
+        ],
+        watch: true,
+        onError(error) {
+            console.log("error: ", error)
+        },
+        onSuccess(data) {
+            console.log("success! --> ", data)
+        }  
+    })
+
+    const currentReadData = data ? data : ""
+    const currentReadPrice = data ? `${utils.formatEther(BigNumber.from(currentReadData[4]).toString())}` + " ETH" : "undefined"
+    const currentFindersFee = data ? `${currentReadData[3] / 100 }` + " %" : "undefined"
+
+    const listingCheck = (sellerAddress) => {
+        if (sellerAddress === "0x0000000000000000000000000000000000000000") {
+            return (
+                <div className="flex flex-row content-center ">
+                No Active Listing for current address + token id
+                </div>
+            )
+        } else {
+            return (
+                <div className="flex flex-row flex-wrap w-fit space-y-1">
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Contract Address: " + nft.nft.nft.contractAddress}
+                    </div>                
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Token Id: " + nft.nft.nft.tokenId}
+                    </div>                
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Seller: " + currentReadData[0]}
+                    </div>
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Funds Recipient: " + currentReadData[1]}
+                    </div>
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Currency: " + currentReadData[2]}
+                    </div>
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Finders Fee: " + currentFindersFee}
+                    </div>
+                    <div className="flex flex-row flex-wrap w-full">                    
+                        {"Price: " + currentReadPrice}
+                    </div>
+                </div>
+            )
+        }
+    }
+    
+    return (
+        <div className=" text-white text-sm h-full flex flex-col flex-wrap items-center justify-center">
+            <main className=" w-full flex flex-row flex-wrap">        
+                {listingCheck(currentReadData[0])}
+            </main>
+        </div>
+    )
+}
---
+++ components/Offers/CancelOffer.tsx
@@ -0,0 +1,109 @@
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/solid'
+import { AskForNFT } from './AskForNFT'
+import { Dispatch, useState, SetStateAction } from 'react'
+
+interface nftInfo {
+    nft: {
+        contractAddress: string,
+        tokenId: string
+    }
+}
+
+export default function AskRead_disclosure(nft) {
+    // export default function ContractActions(nft, setAsksCB: Dispatch<SetStateAction<nftInfo>>)
+
+    return (
+        <div className="ml w-full text-black">
+            <div className=" w-full bg-white ">
+            <Disclosure>
+                {({ open }) => (
+                <>
+                <Disclosure.Button className="hover:bg-[#c3f53b] hover:text-black border-2 px-2 bg-black text-white border-white border-solid flex flex-row w-full justify-between py-2 text-left text-lg font-normal ">
+                    <span>
+                        {"askForNFT(address, tokenId)"}
+                    </span>
+                    {/* <ChevronUpIcon
+                        className={`${
+                        open ? 'rotate-180 transform' : ''
+                        } h-5 w-5`}
+                    /> */}
+                </Disclosure.Button>
+                <Disclosure.Panel className="bg-slate-900 border-white border-2 border-solid flex flex-row flex-wrap items-center p-2 text-lg w-full">
+                    <div className="w-fit flex flex-row flex-wrap">
+                        <AskForNFT nft={nft} />
+                    </div>
+                </Disclosure.Panel>
+                </>
+                )}
+            </Disclosure>
+            </div>
+        </div>
+    )
+}
+
+// return (
+//     <div className=" ml-2 w-full text-black">
+//         <div className=" w-full max-w-md bg-white ">
+//         <Disclosure>
+//             {({ open }) => (
+//             <>
+//             <Disclosure.Button className="hover:bg-white hover:text-black border-2 px-2 bg-black text-white border-white border-solid flex flex-row w-full justify-between py-2 text-left text-lg font-normal ">
+//                 <span>
+//                     {"askForNFT(address, tokenId)"}
+//                 </span>
+//                 {/* <ChevronUpIcon
+//                     className={`${
+//                     open ? 'rotate-180 transform' : ''
+//                     } h-5 w-5`}
+//                 /> */}
+//             </Disclosure.Button>
+//             <Disclosure.Panel className="bg-[#FFAFC2] px-4 pt-4 pb-2 text-lg">
+//                 <div>
+//                     <input
+//                         className="ml-5 text-black text-center bg-slate-200"
+//                         placeholder="contractAddress"
+//                         name="askForNFT_contractAddress"
+//                         type="text"
+//                         value={nft.nft.contractAddress}
+//                         onChange={(e) => {
+//                             e.preventDefault();
+//                             setAsksCB(current => {
+//                                 return {
+//                                 ...current,
+//                                 nft: {
+//                                 ...current.nft,
+//                                 tokenId: e.target.value
+//                                 }}
+//                             })
+//                         }}
+//                         required                              
+//                     >
+//                     </input>
+//                     <input
+//                         className="ml-5 text-black text-center bg-slate-200"
+//                         placeholder="tokenId"
+//                         name="askForNFT_tokenId"
+//                         type="text"
+//                         value={nft.nft.tokenId}
+//                         onChange={(e) => {
+//                             e.preventDefault();
+//                             setAsksCB(current => {
+//                                 return {
+//                                 ...current,
+//                                 tokenId: e.target.value
+//                                 }
+//                             })
+//                         }}
+//                         required                              
+//                     >
+//                     </input>
+//                     <AskForNFT_READ nft={nft} />
+//                 </div>
+//             </Disclosure.Panel>
+//             </>
+//             )}
+//         </Disclosure>
+//         </div>
+//     </div>
+// )
